4f1cfd4109503d6b0a4f538a2c6b213a2fcc9beb
Eric Baranowski <ericbaranowski@futurimedia.com>
2023-08-18 18:08:34 -0400

    added back int types to TransactionStream

--- tap_suiteql/streams.py
+++ tap_suiteql/streams.py
@@ -865,7 +865,7 @@ class TransactionAccountingLineStream(suiteqlStream):
     schema = th.PropertiesList(
         th.Property("account", th.StringType),
         th.Property("accountingbook", th.StringType),
-        th.Property("amount", th.StringType),
+        th.Property("amount", th.NumberType),
         th.Property("amountlinked", th.StringType),
         th.Property("amountpaid", th.StringType),
         th.Property("amountunpaid", th.StringType),
