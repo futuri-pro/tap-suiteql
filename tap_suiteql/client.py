e64999d99937d833c7108318f86483fa60374f93
Eric Baranowski <ericbaranowski@futurimedia.com>
2023-08-23 16:22:38 -0400

    added types to AccountStream

--- tap_suiteql/client.py
+++ tap_suiteql/client.py
@@ -10,22 +10,22 @@ from singer_sdk.streams import RESTStream
 
 from tap_suiteql.auth import suiteqlAuthenticator
 
-try:
-    # For Python 3
-    import http.client as http_client
-except ImportError:
-    # For Python 2 users
-    import httplib as http_client
+# try:
+#     # For Python 3
+#     import http.client as http_client
+# except ImportError:
+#     # For Python 2 users
+#     import httplib as http_client
  
-# Turn verbosity on with debugging
-http_client.HTTPConnection.debuglevel = 1
+# # Turn verbosity on with debugging
+# http_client.HTTPConnection.debuglevel = 1
  
-# You must initialize logging, otherwise you'll not see debug output.
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
+# # You must initialize logging, otherwise you'll not see debug output.
+# logging.basicConfig()
+# logging.getLogger().setLevel(logging.DEBUG)
+# requests_log = logging.getLogger("requests.packages.urllib3")
+# requests_log.setLevel(logging.DEBUG)
+# requests_log.propagate = True
 
 
 class suiteqlStream(RESTStream):
